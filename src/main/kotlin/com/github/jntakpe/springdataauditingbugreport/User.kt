d28fbf38d9dbe7f16a5c7e6deb47f0f76cd10d66
jntakpe <jntakpe@gmail.com>
2018-11-18 18:40:24 +0100

    Simplify

--- src/main/kotlin/com/github/jntakpe/springdataauditingbugreport/User.kt
+++ src/main/kotlin/com/github/jntakpe/springdataauditingbugreport/User.kt
@@ -1,5 +1,23 @@
 package com.github.jntakpe.springdataauditingbugreport
 
 import org.bson.types.ObjectId
+import java.time.LocalDateTime
 
 data class User(val username: String, override val id: ObjectId? = null) : MongoAuditable()
+
+abstract class MongoAuditable: Identifiable {
+
+    companion object {
+        private const val serialVersionUID: Long = 1
+    }
+
+    var createdBy: String? = null
+    var createdAt: LocalDateTime? = null
+    var lastModifiedBy: String? = null
+    var lastModifiedAt: LocalDateTime? = null
+}
+
+interface Identifiable {
+
+    val id: ObjectId?
+}
